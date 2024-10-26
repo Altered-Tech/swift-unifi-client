e43fb11fa9d80ddd0ce6622fb3478cf607d3ed52
Michael Einreinhof <michael@einreinhof.com>
2024-10-25 23:46:50 -0600

    adding cookie and self api path

--- Sources/UnifiClient/UnifiClient.swift
+++ Sources/UnifiClient/UnifiClient.swift
@@ -16,20 +16,20 @@ public struct UnifiClient {
         self.underlyingClient = underlyingClient
     }
     
-    public init(url: String, session: URLSession) {
+    public init(url: String, username: String, password: String, session: URLSession = URLSession(configuration: .default)) async throws {
         let transportConfig = URLSessionTransport.Configuration(session: session)
         let transport = URLSessionTransport(configuration: transportConfig)
         self.init(underlyingClient: Client(serverURL: URL(string: url)!, transport: transport))
     }
     
-    public func login(username: String, password: String) async throws -> Components.Schemas.Authentication {
+    public func login(username: String, password: String) async throws -> String? {
         let response = try await underlyingClient.login(
             .init(body: .json(.init(username: username, password: password)))
         )
         switch response {
                 
             case .ok(let value):
-                return try value.body.json
+                return value.headers.SetCookie
             case .forbidden(let error):
                 if let message = try error.body.json.message {
                     throw UnifiError.forbidden(message: message)
--- Sources/UnifiClient/UnifiError.swift
+++ Sources/UnifiClient/UnifiError.swift
@@ -11,13 +11,3 @@ public enum UnifiError: Error {
     case forbidden(message: String)
     case undocumented(statusCode: Int, message: String? = nil)
 }
-//
-//public struct UnifiLoginError: Codable {
-//    let code: String
-//    let message: String
-//    let level: String
-//}
-//
-//enum UrlError: Error {
-//    case invalidURL(String)
-//}
